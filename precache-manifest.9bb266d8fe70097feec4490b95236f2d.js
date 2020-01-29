fbd3333d6672b376ffb237d9bafa642160b96857
hamid <hamid331994@gmail.com>
2020-01-29 22:16:47 +0330

    Updates

---
+++ precache-manifest.9bb266d8fe70097feec4490b95236f2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498fa642b3dd18ce4578917cd8e6a30d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "835a572a26b9f269c3bf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ef7693ef42017d071f8",
+    "url": "/static/js/2.03fb37cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.03fb37cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "835a572a26b9f269c3bf",
+    "url": "/static/js/main.55421dd3.chunk.js"
+  },
+  {
+    "revision": "bf7f065bf4bea9caa51d",
+    "url": "/static/js/runtime-main.9d448d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
